ec60d6d30600b9e4f53c6e1db02c4820439a7ef3
RobyPurbaSakty26 <obykao26@gmail.com>
2023-05-05 13:17:58 +0700

    one dir

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const produk = require("./contollers");
 
 app.use(express.json());
 
-app.get("/", produk.produk.handleRoot);
+app.get(
+  "/",
+
+  (req, res) => {
+    res.status(200).json({
+      status: "Success",
+      message: "Backend is running",
+    });
+  }
+);
 // app.get("/produk", produk.produk.handlerGetProduk);
 
 app.listen(port, () => {
