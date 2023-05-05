d7f047a06dae8c2cadc690fcddb1a34d3b103f40
RobyPurbaSakty26 <obykao26@gmail.com>
2023-05-05 13:12:44 +0700

    disable produk

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const produk = require("./contollers");
 app.use(express.json());
 
 app.get("/", produk.produk.handleRoot);
-app.get("/produk", produk.produk.handlerGetProduk);
+// app.get("/produk", produk.produk.handlerGetProduk);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
